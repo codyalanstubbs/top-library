0c5464fb2fe51d2cd11f7a3035554990a3c77ee3
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-10-07 15:48:06 -0400

    Add dynamic book tile generation after user input

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ let myLibrary = [
 ];
 
 const bookShelf = document.querySelector(".book-shelf");
+let numBooksOnShelf = document.querySelectorAll(".book").length;
 const addBtn = document.querySelector(".add-book-btn");
-const title = document.getElementsByName("title");
 
+const title =document.querySelector("#title");
+const author = document.querySelector("#author");
+const radioResponses = document.getElementsByName('read');
+const category = document.querySelector("#category");
+const subcategory = document.querySelector("#subcategory");
+const found = document.querySelector("#found");
+const notes = document.querySelector("#notes");
 
 function Book(title, author, read, category, subcategory, found, notes) {
     this.title = title;
@@ -18,16 +25,32 @@ function Book(title, author, read, category, subcategory, found, notes) {
 }
 
 function addBookToLibrary(title, author, read, category, subcategory, found, notes) {
-    myLibrary.push(
-        new Book(title, author, read, category, subcategory, found, notes)
-    )
+    // Add book to library array
+    let newBook = new Book(title, author, read, category, subcategory, found, notes);
+    myLibrary.push(newBook)
+    
+    // Build book container and add to DOM
+    let bookDiv = document.createElement("div");
+    bookDiv.className = "book";
+    numBooksOnShelf += 1;
+    bookDiv.setAttribute('id', "book-" + numBooksOnShelf);
+    bookShelf.appendChild(bookDiv);
+
+    // Add book details to book container
+    let newlyAddedBook = document.querySelector("#book-" + numBooksOnShelf);
+    let bookKeys = Object.keys(newBook);
+    for (let i = 0; i < Object.keys(newBook).length; i++) {
+        let bookDetail = document.createElement("div");
+        bookDetail.className = bookKeys[i];
+        bookDetail.textContent = newBook[bookKeys[i]];
+        newlyAddedBook.appendChild(bookDetail);
+    }
 }
 
 
 addBtn.addEventListener('click', (e) => {
 
     // Find the radio response
-    let radioResponses = document.getElementsByName('read');
     let radioResult;
 
     for (let i = 0; i < radioResponses.length; i++) {
@@ -38,12 +61,10 @@ addBtn.addEventListener('click', (e) => {
 
     // Add book to library
     addBookToLibrary(
-        document.querySelector("#title").value, 
-        document.querySelector("#author").value, 
-        radioResult, 
-        document.querySelector("#category").value, 
-        document.querySelector("#subcategory").value, 
-        document.querySelector("#found").value, 
-        document.querySelector("#notes").value
-    )
+        title.value, author.value, radioResult, 
+        category.value, subcategory.value, found.value, 
+        notes.value
+    );
+
+
 })
