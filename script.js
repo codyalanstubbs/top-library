cffca993851083b02801988a7a55e47c3bcbdb81
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-10-07 15:03:15 -0400

    Add logic to get radio input

--- script.js
+++ script.js
@@ -25,10 +25,22 @@ function addBookToLibrary(title, author, read, category, subcategory, found, not
 
 
 addBtn.addEventListener('click', (e) => {
+
+    // Find the radio response
+    let radioResponses = document.getElementsByName('read');
+    let radioResult;
+
+    for (let i = 0; i < radioResponses.length; i++) {
+        if (radioResponses[i].checked) {
+            radioResult = radioResponses[i].value;
+        }
+    }
+
+    // Add book to library
     addBookToLibrary(
         document.querySelector("#title").value, 
         document.querySelector("#author").value, 
-        document.querySelector("#author").value, 
+        radioResult, 
         document.querySelector("#category").value, 
         document.querySelector("#subcategory").value, 
         document.querySelector("#found").value, 
